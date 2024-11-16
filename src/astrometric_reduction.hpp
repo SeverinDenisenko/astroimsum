b04ec346ae157355fe1105fa0bf16d7a36be48b7
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2024-11-16 04:25:05 +0300

    Add triangle dependency

--- src/astrometric_reduction.hpp
+++ src/astrometric_reduction.hpp
@@ -1,5 +1,28 @@
 #pragma once
 
+#include <functional>
+
+#include "types.hpp"
+
+using real_t = float;
+
+struct coord_t {
+    real_t x;
+    real_t y;
+};
+
+using pixel_transform_t = std::function<coord_t(coord_t)>;
+
+class source_extractor {
+public:
+    array_t<coord_t> extract();
+};
+
+class source_matcher {
+public:
+    pixel_transform_t match(array_t<coord_t> from, array_t<coord_t> to);
+};
+
 class ireduction {
 public:
 };
