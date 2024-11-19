daaea1e9f11a1fdff87d59c602a9639cbda7ac45
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2024-11-19 18:58:08 +0300

    Add run script on test data

---
+++ scripts/run_basic_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo $PWD
+ls $PWD
+ls ./data
+./build/imsum "$PWD"/data/2015_HB10_H_alpha_005.fits 9 \
+    "$PWD"/data/2015_HB10_H_alpha_001.fits \
+    "$PWD"/data/2015_HB10_H_alpha_002.fits \
+    "$PWD"/data/2015_HB10_H_alpha_003.fits \
+    "$PWD"/data/2015_HB10_H_alpha_004.fits \
+    "$PWD"/data/2015_HB10_H_alpha_006.fits \
+    "$PWD"/data/2015_HB10_H_alpha_007.fits \
+    "$PWD"/data/2015_HB10_H_alpha_008.fits \
+    "$PWD"/data/2015_HB10_H_alpha_009.fits \
+    "$PWD"/data/2015_HB10_H_alpha_010.fits
+ls -laht out.fits
