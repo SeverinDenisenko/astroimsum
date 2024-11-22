e94a6ddd9454015d6e0b5e23a7008723dab89e7f
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2024-11-22 22:35:11 +0300

    Implement C API for astroimsum

--- c/astroimsumlib.cpp
+++ c/astroimsumlib.cpp
@@ -1,48 +1,147 @@
-#include "frame.hpp"
-#include "sumation.hpp"
+#include <exception>
 
-struct astroimsumlib { };
-struct astroimsum_frame { };
+#include "frame.hpp"
+#include "frame_io.hpp"
+#include "sumation.hpp"
+#include "types.hpp"
+
+using namespace astro;
+
+struct astroimsum { };
+
+struct astroimsum_frame {
+    frame frame;
+    uptr<iframeloader> frameloader;
+};
 
 extern "C" {
 
-int astroimsum_init(struct astroimsum* handle)
+#include "astroimsumlib.h"
+
+int astroimsum_init(struct astroimsum** handle)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        *handle = new astroimsum();
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 
 int astroimsum_destroy(struct astroimsum* handle)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        delete handle;
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 
-int astroimsum_create_frame(struct astroimsum_frame** frame_out, char* name_in)
+int astroimsum_create_frame(
+    struct astroimsum* handle_in [[maybe_unused]],
+    struct astroimsum_frame** frame_out,
+    char* name_in)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        *frame_out = new astroimsum_frame();
+
+        string_t name(name_in);
+        array_t<string_t> names { name };
+        (*frame_out)->frameloader = make_uptr<batch_frameloader>(names);
+        (*frame_out)->frame       = (*frame_out)->frameloader->get_frame();
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 
 int astroimsum_access_frame(
+    struct astroimsum* handle_in [[maybe_unused]],
     struct astroimsum_frame* frame_in,
     int* width_out,
     int* height_out,
-    float** data_out)
+    unsigned long** data_out)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        *width_out  = frame_in->frame.x_size();
+        *height_out = frame_in->frame.y_size();
+        *data_out   = frame_in->frame.data().data();
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 
-int astroimsum_destroy_frame(struct astroimsum_frame* frame_in)
+int astroimsum_destroy_frame(
+    struct astroimsum* handle_in [[maybe_unused]],
+    struct astroimsum_frame* frame_in)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        frame_in->frameloader = nullptr;
+        frame_in->frame       = frame();
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 
-int astroimsum_write_frame(struct astroimsum_frame* frame_in, char* name_in)
+int astroimsum_write_frame(
+    struct astroimsum* handle_in [[maybe_unused]],
+    struct astroimsum_frame* frame_in,
+    char* name_in)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        string_t name(name_in);
+        sptr<iframesaver> framesaver = make_sptr<casual_framesaver>(name);
+        framesaver->save(frame_in->frame);
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 
 int astroimsum_add_frames(
-    struct astroimsum_frame* add_to, struct astroimsum_frame* add_what)
+    struct astroimsum* handle_in [[maybe_unused]],
+    struct astroimsum_frame* add_to,
+    struct astroimsum_frame* add_what)
 {
-    return 0;
+    int res = 0;
+
+    try {
+        sptr<isumattor> sumattor
+            = make_sptr<basic_star_sumattor>(add_to->frame);
+        sumattor->sum(add_what->frame);
+        add_to->frame = sumattor->result();
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        res = 1;
+    }
+
+    return res;
 }
 }
--- c/include/astroimsumlib.h
+++ c/include/astroimsumlib.h
@@ -7,21 +7,38 @@ struct astroimsum_frame;
 /*
  * Create and destroy astroimsum handle
  */
-int astroimsum_init(struct astroimsum* handle);
-int astroimsum_destroy(struct astroimsum* handle);
+int astroimsum_init(struct astroimsum** handle_out);
+int astroimsum_destroy(struct astroimsum* handle_in);
 
 /*
  * Routine for creation, deletion and access to frames
- * `float* data` must exists all liftime of the frame
  */
-int astroimsum_create_frame(struct astroimsum_frame** frame_out, char* name_in);
-int astroimsum_access_frame(struct astroimsum_frame* frame_in, int* width_out, int* height_out, float** data_out);
-int astroimsum_destroy_frame(struct astroimsum_frame* frame_in);
-int astroimsum_write_frame(struct astroimsum_frame* frame_in, char* name_in);
+int astroimsum_create_frame(
+    struct astroimsum* handle_in,
+    struct astroimsum_frame** frame_out,
+    char* name_in);
+
+int astroimsum_access_frame(
+    struct astroimsum* handle_in,
+    struct astroimsum_frame* frame_in,
+    int* width_out,
+    int* height_out,
+    unsigned long** data_out);
+
+int astroimsum_destroy_frame(
+    struct astroimsum* handle_in, struct astroimsum_frame* frame_in);
+
+int astroimsum_write_frame(
+    struct astroimsum* handle_in,
+    struct astroimsum_frame* frame_in,
+    char* name_in);
 
 /*
  * Main routine for frame summation
  */
-int astroimsum_add_frames(struct astroimsum_frame* add_to, struct astroimsum_frame* add_what);
+int astroimsum_add_frames(
+    struct astroimsum* handle_in,
+    struct astroimsum_frame* add_to,
+    struct astroimsum_frame* add_what);
 
 #endif // ASTROIMSUMLIB_H
